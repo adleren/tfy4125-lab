f7b3d7ac73dfda6d911187e74b789cbfb9d6904c
sigursl <sigursl@stud.ntnu.no>
2019-03-06 11:18:47 +0100

    plot all data in one graph

--- run.py
+++ run.py
@@ -5,15 +5,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Max utslag
-coeffs = iptrack('data/max/track1')
+coeffsMAX = iptrack('data/max/track1')
+
+coeffsTauto = iptrack('data/Tautochron/Tautochron.txt')
+
+coeffsskraa = iptrack('data/Skråplan/Skråplan.txt')
 
 
 
-def f(x):
-	return trvalues(coeffs, x)[4]
+
+
+def f(x, coeffs):
+	return trvalues(coeffs, x)[0]
 
 x = np.linspace(0, 1.3, 101)
 
-plt.plot(x, f(x))
+plt.plot(x, f(x,coeffsskraa))
+plt.plot(x,f(x,coeffsMAX))
+plt.plot(x,f(x,coeffsTauto))
 plt.show()
-	
