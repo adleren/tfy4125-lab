e38b97d8aca22060b170bb175ee1392fc306dd23
sigursl <sigursl@stud.ntnu.no>
2019-03-19 15:15:56 +0100

    add data and change iptrack,run.py

--- run.py
+++ run.py
@@ -5,22 +5,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Max utslag
-coeffsMAX = iptrack('data/max/avg')
+coeffsMAX = iptrack('data/max/testing2')
 
 coeffsTauto = iptrack('data/Tautochron/Tautochron.txt')
 
 coeffsskraa = iptrack('data/Skråplan/Skråplan.txt')
 
 
-
-
-
 def f(x, coeffs):
-	return trvalues(coeffs, x)[0]
+    return trvalues(coeffs, x)[0]
 
-x = np.linspace(0, 1.3, 101)
 
-plt.plot(x, f(x,coeffsskraa))
-plt.plot(x,f(x,coeffsMAX))
-plt.plot(x,f(x,coeffsTauto))
+x = np.linspace(0.022, 1.3, 101)
+
+plt.plot(x, f(x, coeffsskraa))
+plt.plot(x, f(x, coeffsMAX))
+plt.plot(x, f(x, coeffsTauto))
+plt.legend([r'$Skråplan$', r'$Maxs utslag$', r'$Tautokron$'])
+plt.title(r'Plot av x(t)')
+plt.xlabel(r't/s')
+plt.ylabel(r'x/m')
 plt.show()
